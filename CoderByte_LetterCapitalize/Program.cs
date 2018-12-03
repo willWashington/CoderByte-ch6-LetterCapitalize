483570dded4f8d2fa78f5596c3cf578aa75e28e5
w2 <william.washington@live.com>
2018-12-02 20:42:30 -0500

    Add project files.

---
+++ CoderByte_LetterCapitalize/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace CoderByte_LetterCapitalize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Using the C# language, have the function LetterCapitalize(str) take the str parameter being passed and capitalize the first letter of each word. 
+            //Words will be separated by only one space. 
+
+            Console.WriteLine(LetterCapitalize("hello my name is pants"));
+            Console.ReadLine();
+        }
+
+        static string LetterCapitalize(string str)
+        {
+            string capitalized = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(str);
+            return capitalized;
+        }
+    }
+}
